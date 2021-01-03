1769bba8ffab3d13349625c99c378626e0aa9f43
Yashbansal411 <yashbansal411@gmail.com>
2021-01-03 18:28:31 +0530

    created basic api structure

--- README.txt
+++ README.txt
@@ -15,4 +15,33 @@
 11. After make migrations we need to migrate the changes to reflect the changes
       command - "python manage.py migrate"
 12. local/port/admin - open admin page
-13. To create a super user use command - "python manage.py createsuperuser"
+13. To create a super user use command - "python manage.py createsuperuser".
+14. cross origin requests in django
+    14.1 install using pipenv command - pipenv install django-cors-headers
+    14.2 open settings.py and do following changes
+         14.2.1 add line to INSTALLED_APPS=[]
+         14.2.2 add line to MIDDLEWARE = []
+         14.2.3 set CORS_ALLOW_ALL_ORIGINS to True
+                 link of docs - https://pypi.org/project/django-cors-headers/
+15. Install Django rest framework
+     15.1 pipenv install djangorestframework and open settings.py
+     15.2 add rest_framework and rest_framework.authtoken to installed apps.
+     15.3 add rest_framework dictionary in file from docs
+     15.4 open api_guide from documentation link and open Authentication
+     15.5 and copy default_authentication_class in rest_frame_work dict
+     15.6 add custom line in default_dict to enable token based auth.
+     15.7 open urls.py and do changes
+
+16. Add media files
+    16.1 create folder named as media
+    16.2 add media_url and media_root in setting file
+    16.3 import setting and add url_pattern
+
+17 Create new app as api
+   17.1 create new django api by using django-admin startapp "api", api - name, keep the name in lowercase
+   17.2 create subapps like category, user, product, payment, order
+   17.3 as these are also apps we need to add it into settings file 'installed_apps'
+   17.4 add url of api
+   17.5 inside api create views.py and write it from scratch
+   17.6 create home() in views and add it in url 
+
--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 't(ro%=w$7a_h=pw@qill(%uj)cozofpzs^x*petw(fxm(+c1^2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders', #to use cors
+    'rest_framework', # to use jsons
+    'rest_framework.authtoken', # to add custom signups
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'ecom.urls'
@@ -118,3 +125,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication', # to allow token based authentication
+    ]
+}
